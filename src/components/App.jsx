561368374bf7c83702f12056905ce6929262ad3a
michael <747391119@qq.com>
2019-09-04 15:53:11 +0800

    事件方法使用

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,10 @@ import Nav from './Nav'
 
 function App() {
   return (
-        <ul>
-           {
-             [1,2,3,4,5].map((element,index) => {
-                return(
                 <div>
-                  <li>{ element }</li>
                   <Nav title='音乐'/>
                   <Nav title='地址'/>
                 </div>  
-                )
-             })
-           }
-        </ul>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 export default class Nav extends React.Component{
+
+    outText(tx){
+        console.log("打印数据为"+tx)
+    }
+
     render(){
         return(
         <div>
             <p>{ this.props.title }</p>
+            <button onClick={ (e) => this.outText("传递值",e)  }>点我获取</button>
             <ul>
                 <li>导航一</li>
                 <li>导航二</li>

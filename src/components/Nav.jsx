aebe8b457489348d9c78d058b29566c29cfc249f
michael <747391119@qq.com>
2019-09-04 16:16:15 +0800

    条件渲染及列表渲染

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,21 +2,49 @@ import React from 'react'
 
 export default class Nav extends React.Component{
 
+
     outText(tx){
         console.log("打印数据为"+tx)
     }
 
+    outTextX = () => {
+        console.log(this)
+    }
+
     render(){
+        const info = [
+            {
+                id:1,
+                name:"mic",
+                age:10
+            },
+            {
+                id:2,
+                name:"bai",
+                age:20
+            },
+            {
+                id:3,
+                name:"skime",
+                age:30
+            }
+        ]
+        const flag = true;
         return(
-        <div>
-            <p>{ this.props.title }</p>
-            <button onClick={ (e) => this.outText("传递值",e)  }>点我获取</button>
-            <ul>
-                <li>导航一</li>
-                <li>导航二</li>
-                <li>导航三</li>
-            </ul>
-        </div>
+            <div>
+                <p>{ this.props.title }</p>
+                <button onClick={ (e) => this.outText("传递值",e)  }>点我获取</button>
+                <button onClick={ this.outTextX }>点我获取二</button>
+                { flag ? <div>渲染成功</div> : <div>等待数据中...</div> }
+                { info.map((element,index) => {
+                return(    
+                    <ul key={element.id}>
+                    <p>{ element.name }</p>
+                    <p>{ element.age }</p>
+                    </ul>
+                )
+                }) }
+            </div>
         )
     }
 }

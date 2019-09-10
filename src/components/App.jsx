b862b9c5b58d6b02ee5d3ea7179665263d2a082e
michael <747391119@qq.com>
2019-09-10 10:42:02 +0800

    setState异步及同步 处理方式

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,13 +16,23 @@ class App extends React.Component{
     }
   }
 
-  componentDidMount(){
+  async componentDidMount(){
     api.indexMovement.getIndexMovement()
     .then((res) => { return res.json(); })
     .then((data) => {console.log(data)});
     api.indexOther.getOther()
     .then((res) => { return res.json(); })
     .then((data) => {console.log(data)})
+
+    //setState 在控制内时属于异步，这时处理方案可以通过es6 async及await配合使用  
+    //或者使用setState第二个参数，回调函数中进行下一步操作
+    
+    await this.setState({
+       title : "change"
+    })
+
+    console.log(this.state.title);
+
   }
 
   clickHander = () => {
